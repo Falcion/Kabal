abbf25f66c5bae5e4f4b41bfa814ed37606a9db9
Falcion <57592842+Falcion@users.noreply.github.com>
2022-04-19 04:24:41 +0300

    Implemented null module of RANDOM.ORG

---
+++ app/source/MathFX/Randomizer.cs
@@ -0,0 +1,23 @@
+﻿namespace MathFX
+{
+
+    public static class Randomizer
+    {
+
+        ///
+        public static class API_RANDOM_ORG
+        {
+
+            ///
+            public static class Client
+            {
+
+            }
+
+            public static Client Registry()
+            {
+
+            }
+        }
+    }
+}
