bbd1614af493c7d29067bba72c51ed139073a2e8
Falcion <57592842+Falcion@users.noreply.github.com>
2022-04-19 04:25:45 +0300

    Implement future text module

--- app/source/MathFX/Algorithms/Texts.cs
+++ app/source/MathFX/Algorithms/Texts.cs
@@ -4,113 +4,40 @@
     public static class Texts
     {
 
-        /// <summary>
-        ///     Function counting vowels in given string
-        /// </summary>
-        /// 
-        /// <returns>
-        ///     Number of vowels in default latin alphabet
-        /// </returns>
-        /// 
-        public static int CountVowels(string param)
+        ///
+        public static string Palindrome(string input) { return ""; }
+
+        public static string CountVowels(string input) { return ""; }
+
+        public static string CountConsonants(string input) { return ""; }
+
+        public static string CountWords(string input) { return ""; }
+
+
+
+        public static class Cyphers
         {
-            string CHARS_DICT = "aeiou";
 
-            int counter = 0;
-
-            param = param.ToLower();
-
-            foreach (char P_CHAR in param)
+            ///
+            public static string Caesers(string input)
             {
-                if (CHARS_DICT.Any(C => C == P_CHAR))
-                    counter++;
+                return "";
             }
 
-            return counter;
-        }
-
-        /// <summary>
-        ///     Function counting consonants in given string
-        /// </summary>
-        /// 
-        /// <returns>
-        ///     Number of consonants in default latin alphabet
-        /// </returns>
-        /// 
-        public static int CountConsonants(string param)
-        {
-            string CHARS_DICT = "bcdfghjklmnpqrstvwxyz";
-
-            int counter = 0;
-
-            param = param.ToLower();
-
-            foreach (char P_CHAR in param)
+            public static string Piggies(string input)
             {
-                if (CHARS_DICT.Any(C => C == P_CHAR))
-                    counter++;
+                return "";
             }
 
-            return counter;
-        }
-
-        /// <summary>
-        ///     Function counting backspaces in given string
-        /// </summary>
-        /// 
-        /// <returns>
-        ///     Number of backspaces in given string
-        /// </returns>
-        /// 
-        public static int CountSpaces(string param)
-        {
-            int counter = 0;
-
-            foreach(char P_CHAR in param)
+            public static string Binaries(string input)
             {
-                if(P_CHAR == ' ')
-                    counter++;
+                return "";
             }
 
-
-            return counter;
-        }
-
-        /// <summary>
-        ///     Function counting words in given string
-        /// </summary>
-        /// 
-        /// <returns>
-        ///     Number of words which are defined by cutting by spaces
-        /// </returns>
-        /// 
-        public static int CountWords(string param)
-        {
-            int counter = 0;
-
-            string[] ONE_WORDS = new string[] 
-            { 
-                "A", "a", 
-                "I", "i" 
-            };
-
-            string[] enumParams = param.Split(' ');
-
-            foreach(string enumParam in enumParams)
+            public static string ASCII(string input)
             {
-                if(enumParam.Length <= 1)
-                {
-                    if (ONE_WORDS.Any(W => W == enumParam))
-                        counter++;
-                }
-                else
-                {
-                    if(enumParam != " " || enumParam != null)
-                        counter++;
-                }
+                return "";
             }
-
-            return counter;
         }
     }
 }
