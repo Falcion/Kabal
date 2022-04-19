5d4f1fab127fe987177191d7dfb91a8657c56d46
Falcion <57592842+Falcion@users.noreply.github.com>
2022-04-19 04:24:26 +0300

    Implemented null module of graphs

---
+++ app/source/MathFX/Algorithms/Graphs.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathFX.Algorithms
+{
+    public static class Graphs
+    {
+
+        ///
+        public static int Eulers()
+        {
+            return 0;
+        }
+
+        ///
+        public static int Dijkstra()
+        {
+            return 0;
+        }
+    }
+}
